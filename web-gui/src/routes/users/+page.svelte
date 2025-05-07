c20db228b3049d04dd01f06af2c598b247e62af4
Joan Coronado <joan15corza@gmail.com>
2025-05-07 13:54:51 -0600

    refactor(gui): Eliminando codigo inecesario

--- web-gui/src/routes/users/+page.svelte
+++ web-gui/src/routes/users/+page.svelte
@@ -29,38 +29,18 @@
                 </p>
             </div>
 
-            <HorizontalList>
-                <button
-                    class="btn btn-primary"
-                    on:click={() => (isOpen = true)}
+            <button class="btn btn-primary" on:click={() => (isOpen = true)}>
+                <svg
+                    class="w-4 h-4 mr-1"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
                 >
-                    <svg
-                        class="w-4 h-4 mr-1"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                    >
-                        <path
-                            d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
-                        />
-                    </svg>
-                    Agregar Usuario
-                </button>
-                <button
-                    class="btn btn-primary"
-                    on:click={() => (isOpen = true)}
-                >
-                    <svg
-                        class="w-4 h-4 mr-1"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                    >
-                        <path
-                            d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
-                        />
-                    </svg>
-                    Agregar Usuario
-                </button>
-            </HorizontalList>
+                    <path
+                        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+                    />
+                </svg>
+                Agregar Usuario
+            </button>
         </HorizontalList>
     </HeaderContainer>
 
